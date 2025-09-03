e53ae8eb4bec5136c5b9a4e37349a8a2678b437e
lislenytorquato <lislenytorquato19@gmail.com>
2025-09-03 16:26:50 -0300

    feat: Adding Local class

---
+++ src/main/java/com/crud/eventos/model/Local.java
@@ -0,0 +1,23 @@
+package com.crud.eventos.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @ToString.Exclude
+    private Long id;
+    private String nome;
+    private String endereco;
+    private int capacidade;
+    @OneToOne(mappedBy = "local_id")
+    private Evento evento;
+}
