902ce472f2c086df7fc0aded6ce2be13def0efd8
lislenytorquato <lislenytorquato19@gmail.com>
2025-09-03 16:25:11 -0300

    feat: Adding Evento class

---
+++ src/main/java/com/crud/eventos/model/Evento.java
@@ -0,0 +1,32 @@
+package com.crud.eventos.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @ToString.Exclude
+    private Long id;
+    private String nome;
+    private String descricao;
+    private LocalDate data;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "local_id")
+    private Local local;
+    @OneToMany(mappedBy = "participante", cascade = CascadeType.ALL)
+    private List<EventoParticipante> eventosParticipantes = new ArrayList<>();
+
+}
