2f7bfe36ed0696e7534280b6b56e2f8a3485ce26
lislenytorquato <lislenytorquato19@gmail.com>
2025-09-03 17:10:55 -0300

    feat: Adding dto

---
+++ src/main/java/com/crud/eventos/dto/EventoResponsetDto.java
@@ -0,0 +1,19 @@
+package com.crud.eventos.dto;
+
+import com.crud.eventos.model.EventoParticipante;
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class EventoResponsetDto {
+
+    private String nome;
+    private String descricao;
+    private LocalDate data;
+    private Local local;
+    private List<EventoParticipante> eventosParticipantes = new ArrayList<>();
+}
