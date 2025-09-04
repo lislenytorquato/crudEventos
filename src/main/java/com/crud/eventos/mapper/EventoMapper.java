889186c76fdfcafa0f05427be6f558ec3f5ea140
lislenytorquato <lislenytorquato19@gmail.com>
2025-09-04 11:48:30 -0300

    feat: Adding mapper

---
+++ src/main/java/com/crud/eventos/mapper/EventoMapper.java
@@ -0,0 +1,22 @@
+package com.crud.eventos.mapper;
+
+import com.crud.eventos.dto.EventoRequestDto;
+import com.crud.eventos.dto.EventoResponsetDto;
+import com.crud.eventos.model.Evento;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface EventoMapper {
+
+    EventoMapper INSTANCE = Mappers.getMapper(EventoMapper.class);
+
+    Evento requestToEntity(EventoRequestDto eventoRequestDto);
+    EventoResponsetDto entityToResponse(Evento evento);
+    List<EventoResponsetDto> listaEntityToListaResponse(List<Evento> listaDeEventos);
+    void atualizarEvento(@MappingTarget Evento evento,EventoRequestDto eventoRequestDto);
+
+}
